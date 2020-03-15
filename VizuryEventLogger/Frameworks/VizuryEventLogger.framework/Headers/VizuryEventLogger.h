1c935646412ea1f622dad3be7dabcd2c41bf804e
Ayon Chowdhury <md.rajib.sarwar@gmail.com>
2020-03-15 12:23:31 +0530

    adding framework into repo

---
+++ VizuryEventLogger/Frameworks/VizuryEventLogger.framework/Headers/VizuryEventLogger.h
@@ -0,0 +1,19 @@
+//
+//  VizuryEventLogger.h
+//  VizuryEventLogger
+//
+//  Created by Chowdhury Md Rajib  Sarwar on 6/3/20.
+//  Copyright © 2020 Chowdhury Md Rajib  Sarwar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VizuryEventLogger.
+FOUNDATION_EXPORT double VizuryEventLoggerVersionNumber;
+
+//! Project version string for VizuryEventLogger.
+FOUNDATION_EXPORT const unsigned char VizuryEventLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VizuryEventLogger/PublicHeader.h>
+
+
